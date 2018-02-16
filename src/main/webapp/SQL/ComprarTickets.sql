ee0ca1b7f55abe421514ee3da054bfd65acac5d2
Antonio Perez Caballero <antonio.gialnet@gmail.com>
2018-02-16 16:39:27 +0000

    Change procedures for improve Junit Test

--- src/main/webapp/SQL/ComprarTickets.sql
+++ src/main/webapp/SQL/ComprarTickets.sql
@@ -5,17 +5,22 @@
 CREATE OR REPLACE FUNCTION ComprarTicketLlenado(
     xEstanque in integer
 ) 
-returns void
+returns integer
 AS
 $body$
 DECLARE
-
+    xID integer;
 BEGIN
 
     -- Insertar el nuevo ticket
+    WITH BuyTickect_ins AS (
     Insert into Tickets (estanque, canal_compra, tipo) 
-    values (xEstanque, 'web', 'L');
+    values (xEstanque, 'web', 'L')
+    RETURNING ID
+    )
+    select id into xID from BuyTickect_ins;
 
+    return xID;
 END;
 $body$
 LANGUAGE 'plpgsql'
@@ -30,17 +35,23 @@ CREATE OR REPLACE FUNCTION ComprarTicketMinutos(
     xEstanque in integer,
     xMinutos in integer
 ) 
-returns void
+returns integer
 AS
 $body$
 DECLARE
 
-
+    xID integer;
 BEGIN
 
     -- Insertar el nuevo ticket
+    WITH BuyTickect_ins AS (
     Insert into Tickets (estanque, canal_compra, minutos_comprados) 
-    values (xEstanque, 'web', xMinutos);
+    values (xEstanque, 'web', xMinutos)
+    RETURNING ID
+    )
+    select id into xID from BuyTickect_ins;
+
+    return xID;
 
 END;
 $body$
